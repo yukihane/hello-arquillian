74f44b5e8d4575a138df65b9d0702b60734bc56d
yukihane <yukihane.feather@gmail.com>
2016-11-09 11:26:13 +0900

    「Arquillianテストを書く」の初期テストコード

---
+++ arquillian-tutorial/src/test/java/org/arquillian/example/GreeterTest.java
@@ -0,0 +1,11 @@
+package org.arquillian.example;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class GreeterTest {
+    @Test
+    public void should_create_greeting() {
+        Assert.fail("Not yet implemented");
+    }
+}
